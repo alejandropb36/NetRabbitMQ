7ddbf4b96a7f941a21eaba415e0fc0ef42b6f03a
Alejandro Ponce <alexponce36@hotmail.com>
2022-06-06 23:28:49 -0500

    feat: creacion de consumer

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "alejandro",
+    Password = "5r0sc0Ra"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("AlejandroQueue", false, false, false, null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.Span;
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"Mensaje recibido: {message}");
+    };
+
+    channel.BasicConsume("AlejandroQueue", true, consumer);
+
+    Console.WriteLine("Presiona enter para salir del consumer");
+    Console.ReadLine();
+};
