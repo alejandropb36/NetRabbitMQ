96f7d9112f62d4d93f68a906a879ba8c8c5d3f8f
Alejandro Ponce <alexponce36@hotmail.com>
2022-06-06 23:15:51 -0500

    feat: logica de producer

--- Producer/Sender.cs
+++ Producer/Sender.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("AlejandroQueue", false, false, false, null);
+    var message = "Bienvenido al curso de RabbitMQ y .Net";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "AlejandroQueue", null, body);
+    Console.WriteLine("El mensaje fue enviado {0}", message);
+};
+
+Console.WriteLine("Presiona enter para salir de la aplicacion");
+Console.ReadLine();
